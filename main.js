9676eed65a0ca2766c4a0ffd24578aa868068d59
Andrew Branch <andrew@wheream.io>
2017-01-08 14:47:07 -0800

    Add Menu

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const electron = require('electron')
+const shell = electron.shell
+const Menu = electron.Menu
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -6,6 +8,7 @@ const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+const defaultMenu = require('electron-default-menu')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -38,7 +41,11 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow()
+  const menu = defaultMenu(app, shell)
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
